8e600a6c4b4fab8b2bdcb66274116cbde3c32ea3
ralphjaden21-beep <ralphjaden21@gmail.com>
2026-05-05 09:24:47 +0800

    Delete Cabrito'sCakeHouse directory

--- Cabrito'sCakeHouse/logout.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    document.querySelectorAll(".logout-btn").forEach((button) => {
-        button.addEventListener("click", (event) => {
-            event.preventDefault();
-            localStorage.removeItem("cabritoCurrentUser");
-            window.location.href = "Login.html";
-        });
-    });
-})();
